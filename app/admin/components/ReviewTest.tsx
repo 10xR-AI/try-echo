8b0ba7aee67cf317bf73b8fcbc02474a47c86166
NithKon10xR <nithin.konda@10xr.co>
2025-02-18 19:48:09 +0530

    small change

--- app/admin/components/ReviewTest.tsx
+++ app/admin/components/ReviewTest.tsx
@@ -269,7 +269,7 @@ I've prepared some materials that showcase our approach and results. Would you b
                   className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 disabled:opacity-50 disabled:cursor-not-allowed flex items-center gap-2"
                 >
                   {/* <Send className="h-4 w-4" /> */}
-                  {sendingTest ? 'Sending...' : 'Sending Template'}
+                  {sendingTest ? 'Sending...' : 'Send Template'}
                 </button>
               </div>
             </div>
